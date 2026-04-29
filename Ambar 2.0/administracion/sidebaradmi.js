4ef6603bade1bf20ec9d526f80968368ae617273
lian <144626143+lianivan@users.noreply.github.com>
2026-04-29 12:59:00 -0700

    Update sidebaradmi.js

--- Ambar 2.0/administracion/sidebaradmi.js
+++ Ambar 2.0/administracion/sidebaradmi.js
@@ -12,7 +12,7 @@
     ];
 
     const enlacesInferiores = [
-        { texto: 'Soporte',     icono: '❓', url: 'soporte.html', id: 'soporte' },
+        { texto: 'Soporte',     icono: '❓', url: 'soporteM.html', id: 'soporte' },
         { texto: 'Guía de uso', icono: '📖', url: 'guia.html',    id: 'guia' },
     ];
 
